d2dd3b2dac92c3a02951667a7e4c0b61da9e4b78
AhmadAdam38 <ahmadadam0130@gmail.com>
2022-02-25 21:32:33 +0700

    penambahan tombol

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,124 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           SizedBox(height: 24,width: 1,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 25,right: 20),
+                alignment: Alignment.center,
+                width: 150,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      blurRadius: 2,
+                      spreadRadius: 1,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                  child: Text(
+                    '\u002b',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFF2F2F2),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ),
+               Container(
+                margin: EdgeInsets.only(left: 20,right: 25),
+                alignment: Alignment.center,
+                width: 150,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      blurRadius: 2,
+                      spreadRadius: 1,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                  child: Text(
+                    '\u2212',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFF2F2F2),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 25,right: 20,top: 20),
+                alignment: Alignment.center,
+                width: 150,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      blurRadius: 2,
+                      spreadRadius: 1,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                  child: Text(
+                    '\u2a09',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFF2F2F2),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ),
+               Container(
+                margin: EdgeInsets.only(left: 20,right: 25,top: 20),
+                alignment: Alignment.center,
+                width: 150,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.25),
+                      blurRadius: 2,
+                      spreadRadius: 1,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                  child: Text(
+                    '\u2215',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFF2F2F2),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ),
+            ],
+          ),
         ],
       ),
     );
